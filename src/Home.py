1c584f40d7d61f5f8f3f79b2ba79c20429edfe71
pym7857 <ympark4@ls-electric.com>
2024-04-19 18:41:09 +0900

    포트폴리오 매수금액($)만 입력할 수 있도록 수정

--- src/Home.py
+++ src/Home.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 col1, col2 = st.columns(2)
 
-@st.cache_data
+# @st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
@@ -23,12 +23,12 @@ def main():
         # st.subheader('Edit/Add Stock')
         selected_user = st.selectbox('Select User', options=['YM', 'SW'])
         selected_stock = st.text_input('Select Stock to Edit / Add', key='ym1').upper()
-        quantity = st.number_input('Enter Quantity', min_value=0, key='ym2')
-        average_price = st.number_input('Enter Average Price ($)', min_value=100, key='ym3')
+        quantity = st.number_input('Enter Your Amount ($)', min_value=100, key='ym2')
+        # average_price = st.number_input('Enter Average Price ($)', min_value=1, key='ym3')
 
         # 수정/추가 버튼
         if st.button('Edit/Add Stock', key='ym4'):
-            edit_or_add_stock(selected_user, selected_stock, quantity, average_price)
+            edit_or_add_stock(selected_user, selected_stock, quantity)
     with col1:
         st.title('YM')
         
@@ -37,7 +37,7 @@ def main():
         portfolio = load_portfolio(FILE_NAME)
         
         # 포트폴리오에서 종목별 금액 계산
-        portfolio['Amount'] = portfolio['Quantity'] * portfolio['Average Price']
+        # portfolio['Amount'] = portfolio['Quantity'] * portfolio['Average Price']
         
         # 종목별 금액을 사용하여 원 그래프 그리기
         draw_pie_chart(portfolio)
@@ -60,7 +60,7 @@ def main():
         portfolio = load_portfolio(FILE_NAME)
         
         # 포트폴리오에서 종목별 금액 계산
-        portfolio['Amount'] = portfolio['Quantity'] * portfolio['Average Price']
+        # portfolio['Amount'] = portfolio['Quantity'] * portfolio['Average Price']
         
         # 종목별 금액을 사용하여 원 그래프 그리기
         draw_pie_chart(portfolio)
@@ -82,10 +82,10 @@ def load_portfolio(FILE_NAME):
     try:
         portfolio = pd.read_csv(FILE_NAME)
     except FileNotFoundError:
-        portfolio = pd.DataFrame(columns=['Name', 'Quantity', 'Average Price'])
+        portfolio = pd.DataFrame(columns=['Name', 'Quantity'])
     return portfolio
 
-def edit_or_add_stock(selected_user, selected_stock, quantity, average_price):
+def edit_or_add_stock(selected_user, selected_stock, quantity):
     """
     선택한 종목을 수정하거나 새로운 종목을 추가하는 함수
     """
@@ -106,10 +106,10 @@ def edit_or_add_stock(selected_user, selected_stock, quantity, average_price):
             
             portfolio = portfolio[portfolio['Name'] != selected_stock]
         else:
-            portfolio.loc[portfolio['Name'] == selected_stock, ['Quantity', 'Average Price']] = quantity, average_price
+            portfolio.loc[portfolio['Name'] == selected_stock, ['Quantity']] = quantity
     else:
         # 선택한 종목이 포트폴리오에 없는 경우 추가
-        new_stock = pd.DataFrame({'Name': [selected_stock], 'Quantity': [quantity], 'Average Price': [average_price]})
+        new_stock = pd.DataFrame({'Name': [selected_stock], 'Quantity': [quantity]})
         portfolio = pd.concat([portfolio, new_stock], ignore_index=True)
     portfolio = portfolio.drop_duplicates().reset_index(drop=True)
     if not portfolio.empty:
@@ -138,7 +138,7 @@ def draw_pie_chart(portfolio):
     """
     if not portfolio.empty:
         labels = portfolio['Name'].tolist()
-        amounts = portfolio['Amount'].tolist()
+        amounts = portfolio['Quantity'].tolist()
         # colors = np.random.rand(len(amounts), 3)
         colors = [generate_pastel_color() for _ in range(len(amounts))]
         
@@ -162,8 +162,7 @@ def draw_pie_chart(portfolio):
         st.pyplot(fig)
 
         # 전체 포트폴리오의 총 금액 계산
-        portfolio['Total Amount'] = portfolio['Quantity'] * portfolio['Average Price']
-        total_portfolio_amount = portfolio['Total Amount'].sum()
+        total_portfolio_amount = portfolio['Quantity'].sum()
         st.write(f'Total Portfolio Amount: ${total_portfolio_amount:.2f}')
     else:
         st.write('No stocks added yet.')
